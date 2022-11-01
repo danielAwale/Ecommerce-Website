db83f07fb11cb9af1b1609364da62794c6f16339
danielAwale <danielmadhukarawale@gmail.com>
2022-11-01 07:26:53 -0700

    contain more code for the Hero Banner component

--- ecommerce/components/HeroBanner.jsx
+++ ecommerce/components/HeroBanner.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
 const HeroBanner = () => {
   return (
     <div className='hero-banner-container'>
       <div>
         <p className='beats-solo'>SMALL TEXT</p>
+        <h3>MID TEXT</h3>
+        <img src="" alt="headphones" className='hero-banner-image'/>
+
+        <div>
+          <Link href="/product/ID">
+            <button type="button">Button Text</button>
+          </Link>
+        </div>
       </div>
     </div>
   )
