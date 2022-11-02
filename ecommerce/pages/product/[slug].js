06f0e31b01a0e15475568343084dfefa3a0abe41
danielAwale <danielmadhukarawale@gmail.com>
2022-11-02 16:12:20 -0700

    contain a product folder with a slug file in it to dynamically render the products of the specific page and route

---
+++ ecommerce/pages/product/[slug].js
@@ -0,0 +1,11 @@
+// the reason for it to be in a [] is because it will dynamically render it!
+
+import React from 'react'
+
+const ProductDetails = () => {
+  return (
+    <div>ProductDetails</div>
+  )
+}
+
+export default ProductDetails
