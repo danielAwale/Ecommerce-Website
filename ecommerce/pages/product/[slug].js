b543ce13c3d0024b5b1ace9d6def47803922c22d
danielAwale <danielmadhukarawale@gmail.com>
2022-11-02 16:28:11 -0700

    contain a get static props, getting errors; need to debug

--- ecommerce/pages/product/[slug].js
+++ ecommerce/pages/product/[slug].js
@@ -1,11 +1,33 @@
 // the reason for it to be in a [] is because it will dynamically render it!
 
 import React from 'react'
+import { client, urlfor } from '../../lib/client'
 
-const ProductDetails = () => {
+const ProductDetails = ({ product, products }) => {
+  const { image, name, details, price } = product;
   return (
-    <div>ProductDetails</div>
+    <div>
+      <div className='product-detail-container'>
+        <div className='image-contianer'>
+          <img src={urlFor(image && image[0])} />
+        </div>
+      </div>
+
+    </div>
   )
+}
+
+
+
+export const getStaticProps = async ({ params: slug }) => {
+  const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+  const productsQuery = '*[_type == "product"]'
+  const product = await client.fetch(query);
+  const products = await client.fetch(productsQuery)
+
+  return {
+    props: { products, product }
+  }
 }
 
 export default ProductDetails
