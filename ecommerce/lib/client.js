ae19a13bbf7e687b88fe4c86e4f557001d11d45b
danielAwale <danielmadhukarawale@gmail.com>
2022-11-01 20:54:56 -0700

    fix some minor bugs

--- ecommerce/lib/client.js
+++ ecommerce/lib/client.js
@@ -4,7 +4,7 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-  projectID: "an3xgt8i",
+  projectId: "an3xgt8i",
   dataset: "production",
   apiVersion: "2022-03-10",
   useCdn: true,
