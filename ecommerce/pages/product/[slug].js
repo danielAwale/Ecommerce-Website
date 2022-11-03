da503ce7bb85ae39a61ea7a0bab4a2ac79c6b605
danielAwale <danielmadhukarawale@gmail.com>
2022-11-03 07:25:51 -0700

    contain useState and finish the product detail page

--- ecommerce/pages/product/[slug].js
+++ ecommerce/pages/product/[slug].js
@@ -1,28 +1,29 @@
 // the reason for it to be in a [] is because it will dynamically render it!
 
-import React from 'react'
+import React, { useState } from 'react'
 import { client, urlFor } from '../../lib/client'
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
 import { Product } from '../../components';
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
+  const [index, setIndex] = useState(0);
   return (
     <div>
       <div className='product-detail-container'>
         <div>
           <div className='image-contianer'>
-            <img src={urlFor(image && image[0])} />
+            <img src={urlFor(image && image[index])} className="product-detail-image" />
           </div>
-          {/* <div className='small-image-container'>
+          <div className='small-images-container'>
             {image?.map((item, i) => (
               <img
                 src={urlFor(item)}
-                className
-                onMouseEnter={ }
+                className={i === index ? 'small-image selected-image' : 'small-image'}
+                onMouseEnter={() => setIndex(i)}
               />
             ))}
-          </div> */}
+          </div>
         </div>
         <div className='product-detail-desc'>
           <h1>{name}</h1>
